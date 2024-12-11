de90cee4e590cc5638ed3e5d78450fb32fbf332a
lewis <rinatt116@gmail.com>
2024-12-11 17:19:30 +0300

    change routs

--- courses/main/urls.py
+++ courses/main/urls.py
@@ -10,8 +10,7 @@ urlpatterns = [
     path("profile/", views.profile, name="profile"),
     path("courses/", views.courses_overview, name="courses"),
     path("courses/<int:course_id>", views.course_page, name="course_page"),
-    path("courses/<int:course_id>/<int:lesson_id>", views.lesson_page, name="lesson_page"),
-    # path("lesson", views.lesson_page, name="lesson page"),
-    path("tests/<int:course_id>/<int:question_id>", views.question_page, name="question page"),
+    path("courses/<int:course_id>/lesson/<int:lesson_id>", views.lesson_page, name="lesson_page"),
+    path("courses/<int:course_id>/test/<int:question_id>", views.question_page, name="question page"),
     path("test", views.test_page, name="test page")
 ]
