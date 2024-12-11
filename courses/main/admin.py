757f2e223ab89953d8d5d9884e54e85488c6e0a5
lewis <rinatt116@gmail.com>
2024-12-11 04:10:09 +0300

    change json to inner db models

--- courses/main/admin.py
+++ courses/main/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
-from .models import Course, LessonModule, Lesson, QuestionModule, Question, UserAnswer
+from .models import Course, Lessons_Group, Lesson, Questions_Group, Question, UserAnswer
 
 
 # Register your models here.
 admin.site.register(Course)
-admin.site.register(LessonModule)
+admin.site.register(Lessons_Group)
 admin.site.register(Lesson)
-admin.site.register(QuestionModule)
+admin.site.register(Questions_Group)
 admin.site.register(Question)
 admin.site.register(UserAnswer)
--- courses/main/models.py
+++ courses/main/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from datetime import datetime
 from django.contrib.auth.models import User
 from django.utils import timezone
+from django.db.models.functions import Now
 
 """
 От бд нужно:
@@ -24,38 +25,33 @@ questions
 
 class Course(models.Model):
     name = models.CharField(max_length=50)
-    desc = models.CharField(max_length=250)
+    desc = models.CharField(max_length=250, default="")
     author = models.CharField(max_length=50)
+    create_datetime = models.DateTimeField(default=timezone.now)
 
-class LessonModule(models.Model):
+class Lessons_Group(models.Model):
     name = models.CharField(max_length=50)
-    course = models.ForeignKey(Course, related_name='lessons_module', on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, related_name='lessons_group', on_delete=models.CASCADE)
 
 class Lesson(models.Model):
     name = models.CharField(max_length=50)
-    lesson_text = models.CharField(max_length=1000)
-    lesson_module = models.ForeignKey(LessonModule, related_name='lesson', on_delete=models.CASCADE)
+    content = models.CharField(max_length=10000)
+    lesson_group = models.ForeignKey(Lessons_Group, related_name='lesson', on_delete=models.CASCADE)
 
-class QuestionModule(models.Model):
+class Questions_Group(models.Model):
     name = models.CharField(max_length=50)
-    course = models.ForeignKey(Course, related_name='questions_module', on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, related_name='questions_group', on_delete=models.CASCADE)
 
 class Question(models.Model):
     name = models.CharField(max_length=50)
-    question_text = models.CharField(max_length=1000)
-    question_module = models.ForeignKey(QuestionModule, related_name='question', on_delete=models.CASCADE)
+    question_condition = models.CharField(max_length=1000)
+    answers = models.JSONField()
+    correct_answer = models.IntegerField(default=0)
+    question_group = models.ForeignKey(Course, related_name='question', on_delete=models.CASCADE)
 
 class UserAnswer(models.Model):
     user = models.ForeignKey(User, related_name='user_choices', on_delete=models.DO_NOTHING)
     is_correct = models.BooleanField()
+    user_answer = models.IntegerField(default=-1)
+    course = models.ForeignKey(Course, on_delete=models.DO_NOTHING, default=-1)
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-
-# class Questions(models.Model):
-#     question_text = models.CharField(max_length=200)
-#     pub_date = models.DateTimeField("date published")
-
-
-# class Choice(models.Model):
-#     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-#     choice_text = models.CharField(max_length=200)
-#     votes = models.IntegerField(default=0)
